2c35ca5ab872da6a3873b6d3361a13a2ba4f1fce
charleseliemarielle-ops <charleseliemarielle@gmail.com>
2026-06-25 15:37:04 +0200

    Update theme.ts

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,8 +1,1 @@
-// theme.ts
-export const initializeTheme = () => {
-  if (typeof window === "undefined") return;
-  
-  const root = document.documentElement;
-  root.classList.add("dark");
-  root.style.colorScheme = "dark"; // Force aussi les barres de défilement en sombre
-};
+
